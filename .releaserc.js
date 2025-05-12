13a7112d6d20465fd68d4ef095c6e72f312d3ca5
Stanislav Lorents <s.lorents@abysssolutions.com.au>
2025-05-12 12:58:25 +1000

    chore: add semantic release

---
+++ .releaserc.js
@@ -0,0 +1,23 @@
+export default {
+  branches: ['main', { name: 'develop', channel: 'next' }, { name: 'alpha', prerelease: true }],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { revert: true, release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+};
